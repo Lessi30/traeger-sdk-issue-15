5064226d13fcf3493b3818826bbbeda0bab53f09
Alessio Vangelisti <alessio.vangelisti@komaxgroup.com>
2025-01-23 09:16:31 +0100

    Method parameter does not inherit correct from base class

---
+++ Traeger.ComplexParameter/ArtemisNodeManager.cs
@@ -0,0 +1,30 @@
+﻿using Opc.UaFx;
+using Opc.UaFx.Server;
+using Traeger.ComplexParameter.TraegerTypes;
+namespace Traeger.ComplexParameter
+{
+    public class ArtemisNodeManager() : OpcNodeManager("http://artemis.com/Artemis/")
+    {
+        // Works correct when the nodeset is not imported
+        protected override IEnumerable<OpcNodeSet> ImportNodes()
+        {
+            yield return OpcNodeSet.Load(@".\NodeSets\artemis 2.xml");
+        }
+        protected override IEnumerable<IOpcNode> CreateNodes(OpcNodeReferenceCollection references)
+        {
+            base.CreateNodes(references);
+            
+            yield return new OpcDataTypeNode<JobEntryData>();
+            yield return new OpcDataTypeNode<NewJobData>();
+
+            var methods = new OpcFolderNode(this.DefaultNamespace.GetName("Instance"));
+
+            var instance = new JobListObjectNode(methods, "MyJobListInstance", "ns=2;s=JobListInstance", SystemContext);
+
+            references.Add(methods, OpcObjectTypes.ObjectsFolder);
+
+            yield return instance;
+            yield return methods;
+        }
+    }
+}
---
+++ Traeger.ComplexParameter/TraegerTypes/JobListObjectNode.cs
@@ -0,0 +1,53 @@
+﻿using Opc.Ua;
+using Opc.UaFx;
+
+namespace Traeger.ComplexParameter.TraegerTypes
+{
+    internal sealed class JobListObjectNode : OpcObjectNode
+    {
+        public OpcMethodNode AddJobNode { get; }
+
+        #region ---------- Public constructors ----------
+        
+        public JobListObjectNode(IOpcNode parent, OpcName name, OpcNodeId id, OpcContext context)
+            : base(parent, name, id)
+        {
+            AddJobNode = new OpcMethodNode(
+                parent: this,
+                name: "AddJob",
+                id: "ns=2;i=7013",
+                callback: new Func<NewJobData, uint>(AddJob));
+
+            this.AddChild(context, AddJobNode);
+        }
+
+        #endregion
+
+        #region ---------- Protected properties ----------
+
+        protected override OpcNodeId DefaultTypeDefinitionId
+        {
+            get => "ns=2;i=1022";
+        }
+
+        protected override OpcNodeId DefaultReferenceTypeId
+        {
+            get => ReferenceTypeIds.HasTypeDefinition;
+        }
+
+        #endregion
+
+        #region ---------- Private methods ----------
+
+        private uint AddJob(NewJobData job)
+        {
+            return 0;
+        }
+        private (uint Status, List<JobEntryData> Jobs) ListJobs()
+        {
+            return (0, new List<JobEntryData>());
+        }
+
+        #endregion
+    }
+}
